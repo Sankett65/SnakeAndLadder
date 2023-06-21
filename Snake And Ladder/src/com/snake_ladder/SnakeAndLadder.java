1a9dfd5f8ab9da0e926d31c12549c3b788c7ed69
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-06-21 20:10:48 +0530

    Starting point

--- Snake And Ladder/src/com/snake_ladder/SnakeAndLadder.java
+++ Snake And Ladder/src/com/snake_ladder/SnakeAndLadder.java
@@ -1,7 +1,10 @@
 package com.snake_ladder;
 
 public class SnakeAndLadder {
+    public static  int USER=0;
+    
     public static void main(String[] args) {
+
         System.out.println("Welcome to Snake And Ladder Game");
 
     }
